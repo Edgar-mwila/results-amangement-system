1271c085c0e11a866b37c1a2ce089230408492ab
grace kumwenda <gracekumwenda1709@gmail.com>
2025-03-18 04:51:05 -0700

    auth

--- src/routes/auth/register.tsx
+++ src/routes/auth/register.tsx
@@ -3,3 +3,4 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/auth/register')({
   component: () => <div>Hello /auth/register!</div>,
 })
+//parent and teacher registration page
