50e5b508a1347e93092db645da85b1cc22d6a9d5
edgar <edgarmwila84@gmail.com>
2025-06-03 16:57:02 +0200

    fixed minor error.

--- src/routes/$school/dashboard/index.tsx
+++ src/routes/$school/dashboard/index.tsx
@@ -118,7 +118,7 @@ const Layout = () => {
                   }`}
                   onClick={() => setActiveOption(path)}
                 >
-                  <Icon className="w-5 h-5 mr-3" />
+                  <Icon />
                   {label}
                 </Link>
               </li>
